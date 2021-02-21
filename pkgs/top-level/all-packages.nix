ea9dca36eb4b4ec4563688a1c0f99207e7b57938
Attila Lendvai <attila@lendvai.name>
2021-02-21 20:14:44 +0100

    nixos/bee: make it able to start n instances
    also update the packages to the new release, v1.0.0
    
    fix umask when creating the password file (#121293).

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -3391,7 +3391,13 @@ in
     version = "unstable";
   };
 
-  bee-clef = callPackage ../applications/networking/bee/bee-clef.nix { };
+  bee-clef = callPackage ../applications/networking/bee/bee-clef.nix {
+    version = "release";
+  };
+
+  bee-clef-unstable = bee-clef.override {
+    version = "unstable";
+  };
 
   beets = callPackage ../tools/audio/beets {
     pythonPackages = python3Packages;
